590b134e81df2d530ced00c3ae1eab34032fa96b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2024-01-07 20:40:12 +0600

    add InputStream

---
+++ src/InputStream.js
@@ -0,0 +1,52 @@
+/**
+ * @param input {string}
+ * @constructor
+ */
+function InputStream(input) {
+  let pos = 0
+  let line = 1
+  let col = 0
+
+  return {
+    next, peek, eof, raise
+  }
+
+  /**
+   * @public
+   */
+  function next() {
+    const char = input.charAt(pos++)
+
+    if (char === '\n') {
+      line++
+      col = 0
+    } else {
+      col += 1
+    }
+
+    return char
+  }
+
+  /**
+   * @public
+   */
+  function peek() {
+    return input.charAt(pos)
+  }
+
+  /**
+   * @public
+   */
+  function eof() {
+    return peek() === ''
+  }
+
+  /**
+   * @public
+   */
+  function raise(msg) {
+    throw new Error(`${msg} (${line}:${col})`)
+  }
+}
+
+export {InputStream}
